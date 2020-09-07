11c86e40cccebccc1659d5f4799faca2455c1ce2
Chennuri Ullas Reddy <ullasreddy3663@gmail.com>
2020-09-07 11:54:09 +0530

    Updated

--- Arrays/sum_of_submatrices.cpp
+++ Arrays/sum_of_submatrices.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 int submatrix_sum(int a[][1000],int m,int n){
+    //count no of sub matrices in which the cell occured and multiply it with the cell value 
+    // and add it to sum 
+    // Brute Force T.C : O(n^6)
+    // T.C : O(n^2).
+    
     int sum=0;
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
